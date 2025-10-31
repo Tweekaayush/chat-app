5bafec598f11b516f76d0bb8a18dd0a706c73010
Aayush <aayush.dl8901@gmail.com>
2025-10-31 15:03:25 +0530

    add AuthLayout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import AuthLayout from "./layout/AuthLayout";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/login" element={<></>} />
-      <Route path="/signup" element={<></>} />
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<></>} />
+        <Route path="/signup" element={<></>} />
+      </Route>
       <Route path="/" element={<></>} />
       <Route path="/chat/:id" element={<></>} />
     </Routes>
---
+++ client/src/layout/AuthLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { MessageCircleCode } from "lucide-react";
+import { Outlet } from "react-router-dom";
+
+const AuthLayout = () => {
+  return (
+    <div className="w-full h-screen flex flex-col lg:flex-row p-4 bg-white dark:bg-gray-900">
+      <div className="flex items-center justify-start lg:justify-center lg:flex-1 lg:p-8">
+        <div className="">
+          <MessageCircleCode className="text-black dark:text-white w-11 h-11 lg:w-75 lg:h-75"/>
+        </div>
+      </div>
+      <div className="flex items-center justify-center lg:flex-1 lg:p-4">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
