e1c657fce689bbf9ce489218d52a6981cb2e0c2d
Aayush <aayush.dl8901@gmail.com>
2025-11-05 00:00:58 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get(/.*/, (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 } else {
