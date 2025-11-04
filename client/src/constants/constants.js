98d42aecc4c19ab2c75d3ddfdb88242fd1b45baf
Aayush <aayush.dl8901@gmail.com>
2025-11-04 18:33:57 +0530

    fix bugs

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -3,6 +3,6 @@ const BASE_URL =
 
 export const CHAT_API = `${BASE_URL}/api/v1/chat`;
 export const AUTH_API = `${BASE_URL}/api/v1/auth`;
-export const USER_API = `${BASE_URL}/api/v1/auth`;
+export const USER_API = `${BASE_URL}/api/v1/user`;
 
 export default BASE_URL;
