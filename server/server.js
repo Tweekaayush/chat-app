f268af82e3e70aa84a97efcd19f137c3380c9229
Aayush <aayush.dl8901@gmail.com>
2025-11-04 23:03:25 +0530

    fix bugs

--- server/server.js
+++ server/server.js
@@ -45,10 +45,10 @@ app.use("/api/v1/chat", chatRoute);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
